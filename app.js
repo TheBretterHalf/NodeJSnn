ff8b982bef8521374335cafa4659ed94c870e9b7
Brett Stainken <brettstainken.student@careerdevs.com>
2019-01-04 01:13:28 +0000

    Version6.0

--- app.js
+++ app.js
@@ -7,14 +7,25 @@
 
 // console.log('This is an instruction outside the sync file system.');
 
-console.log("Hey Ninjas");
+// console.log("Hey Ninjas");
 
-var time = 0
+// var time = 0
 
-setInterval(function(){
-    time+=2;
-    console.log(time+ ' seconds have passed');
-},2000);
+// setInterval(function(){
+//     time+=2;
+//     console.log(time+ ' seconds have passed');
+// },2000);
 
-console.log(__dirname);
-console.log(__filename)
+// console.log(__dirname);
+// console.log(__filename)
+
+function sayHi(){
+    console.log('hi');
+}
+
+sayHi()
+
+var sayBye = function(){
+    console.log('bye');
+}
+sayBye();
