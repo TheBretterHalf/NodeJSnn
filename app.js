fe822362c83bde43e3f4431367f0435bac1ac858
Brett Stainken <brettstainken.student@careerdevs.com>
2019-01-04 00:49:13 +0000

    Set Interval __dirname, __filename

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-var fs = require('fs');
+// var fs = require('fs');
 
-var file = fs.readFile('input.txt','utf8',function(err,data){
-    fs.writeFile('writeme.txt',data);
-    console.log(data);
-});
+// var file = fs.readFile('input.txt','utf8',function(err,data){
+//     fs.writeFile('writeme.txt',data);
+//     console.log(data);
+// });
 
-console.log('This is an instruction outside the sync file system.');
+// console.log('This is an instruction outside the sync file system.');
+
+console.log("Hey Ninjas");
+
+var time = 0
+
+setInterval(function(){
+    time+=2;
+    console.log(time+ ' seconds have passed');
+},2000);
+
+console.log(__dirname);
+console.log(__filename)
